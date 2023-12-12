e5c5eca60d1a100732a95d97e56867fee8a49165
Luis Chrestia <chrestia.luis@gmail.com>
2023-12-12 07:09:38 -0300

    fix tamaño logo

--- src/components/LogoLengua/index.jsx
+++ src/components/LogoLengua/index.jsx
@@ -7,7 +7,7 @@ export const LogoLengua = () => {
         <div className='allLogo'>
         <div className='container-logo' id='LogoLengua'>
         <div className='img-logo'>
-            <Link to="/"><img className="home-img" src={logo} alt="home-img" width={200}/></Link>
+            <Link to="/"><img className="home-img" src={logo} alt="home-img" width={120}/></Link>
             </div>
         <div className='container-logo-bottom'></div>
         </div>
--- src/pages/peluqueria/index.jsx
+++ src/pages/peluqueria/index.jsx
@@ -1,26 +1,37 @@
 import "./peluqueria.css";
-import { LogoLengua } from "../../components";
+import { BtnHome } from "../../components";
 //import { peluqueriaMain } from "../../assets/images/peluqueriaMain.jpeg"
 
 export const PeluqueriaPage = () => {
     return (
         <>
-            <LogoLengua />
             <div className="container-peluqueria">
                 <section>
                     <div className="peluqueriaPicture">
                         <h2>Bienvenidos</h2>
                     </div>
+                    <h5>Hola!</h5>
                     <p>
-                        Hola! Primero que todo gracias por llegar hasta aquí, mi
+                        Primero que todo gracias por llegar hasta aquí, mi
                         objetivo es este apartado es darte facilidad para que
                         entiendas cómo funciona lengua afuera. Y obvio despejar
                         dudas para dar el paso de confiarnos tu compañero de
                         cuatro patas 🐶😊
                     </p>
                 </section>
-                
+                <section>
+                <ul>
+                    <li>¿Que es lengua afuera?</li>
+                    <li>¿Quien soy? </li>
+                    <li>¿Queres conocer nuestro salón? </li>
+                    <li>¿Cuáles son las pautas de trabajo? </li>
+                    <li>¿Que son los packs de servicio? </li>
+                    <li>¿Donde queda la pelu? </li>
+                    <li>Contacto.</li>
+                    </ul>
+                </section>
             </div>
+            <BtnHome />
         </>
     );
 };
